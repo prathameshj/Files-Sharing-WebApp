22571cbf48e15b82abcea214a9ec453b7fa318d0
prathameshj <63179683+prathameshj@users.noreply.github.com>
2021-05-23 17:19:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const dropZone = document.querySelector(".drop-zone");
+const browseBtn = document.querySelector(".browseBtn");
+const fileInput = document.querySelector("#fileInput");
+
+const host = "https://fshare.herokuapp.com/";
+const uploadURL = `${host}api/files`;
+
+dropZone.addEventListener("dragover", (e)=>{
+    e.preventDefault();
+
+    if (dropZone.classList.contains("dragged")){
+    dropZone.classList.add("dragged");
+ }
+});
+
+dropZone.addEventListener("dragleave", ()=>{
+    dropZone.classList.remove("dragged");
+});
+
+dropZone.addEventListener("drop", (e)=>{
+    e.preventDefault();
+    dropZone.classList.remove("dragged");
+    const files = e.dataTransfer.files;
+    console.table(files);
+    if(files.length){
+        fileInput.files = files;
+        uploadFile();
+    }
+    
+});
+
+browseBtn.addEventListener("click", ()=>{
+    fileInput.click();
+});
+
+const uploadFile = ()=>{
+
+    const file = fileInput.files[0];
+    const formData = new FormData();
+    formData.append("myfile", file);
+
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = ()=>{
+        if (xhr.readyState === XMLHttpRequest.DONE){
+            console.log(xhr.response);
+        }
+        
+    };
+
+    xhr.open("POST", uploadURL);
+    xhr.send(formData);
+};
